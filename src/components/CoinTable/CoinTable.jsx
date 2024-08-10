5864e1085bab42b247337da70e47680618d4c5d9
Sanket Singh <singhsanket143@gmail.com>
2024-08-10 12:59:35 +0530

    Added coin table ui with cache

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchCoinData } from "../../services/fetchCoinData";
 import { useQuery } from "react-query";
 
@@ -6,24 +6,83 @@ function CoinTable() {
 
     const [page, setPage] = useState(1);
     const { data, isLoading, isError, error} = useQuery(['coins', page], () => fetchCoinData(page, 'usd'), {
-        retry: 2,
-        retryDelay: 1000,
+        // retry: 2,
+        // retryDelay: 1000,
         cacheTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60 * 2,
     });
 
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
-
-    if(isLoading) {
-        return <div>Loading...</div>;
-    }
     if(isError) {
         return <div>Error: {error.message}</div>;
     }
     
     return (
-        <>Coin Table <button onClick={() => setPage(page+1)}>Click</button> {page} </>
+        <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto">
+            <div className="w-full bg-yellow-400 text-black flex py-4 px-2 font-semibold items-center justify-center">
+                {/* Header of the table */}
+                <div className="basis-[35%]">
+                    Coin 
+                </div>
+                <div  className="basis-[25%]">
+                    Price 
+                </div>
+                <div  className="basis-[20%]">
+                    24h change 
+                </div>
+                <div  className="basis-[20%]">
+                    Market Cap
+                </div>
+            </div>
+
+            <div className="flex flex-col w-[80vw] mx-auto">
+                {isLoading && <div>Loading...</div>}
+                {data && data.map((coin) => {
+                    return (
+                        <div key={coin.id} className="w-full bg-transparent text-white flex py-4 px-2 font-semibold items-center justify-between">
+                            <div className="flex items-center justify-start gap-3 basis-[35%]">
+
+                                <div className="w-[5rem] h-[5rem]">
+                                    <img src={coin.image} className="w-full h-full" />
+                                </div>
+
+                                <div className="flex flex-col"> 
+                                    <div className="text-3xl">{coin.name}</div>
+                                    <div className="text-xl">{coin.symbol}</div>
+                                </div>
+
+
+                            </div>
+
+                            <div className="basis-[25%]">
+                                {coin.current_price}
+                            </div>
+                            <div className="basis-[20%]">
+                                {coin.price_change_24h}
+                            </div>
+                            <div className="basis-[20%]">
+                                {coin.market_cap}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+
+            <div className="flex gap-4 justify-center items-center">
+                <button
+                    disabled={page === 1}
+                    onClick={() => setPage(page-1)} 
+                    className="btn btn-primary btn-wide text-white text-2xl"
+                >
+                    Prev
+                </button>
+                <button 
+                    onClick={() => setPage(page+1)} 
+                    className="btn btn-secondary btn-wide text-white text-2xl"
+                >
+                    Next
+                </button>
+            </div>
+        </div>
     )
 }
 
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -4,8 +4,7 @@ export async function fetchCoinData(page = 1, currency = 'usd') {
     const perPage = 10;
     try {
         const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
-        console.log(response);
-        return response;
+        return response.data;
 
     } catch(error) {
         console.error(error);
